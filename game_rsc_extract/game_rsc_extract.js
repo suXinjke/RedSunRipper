2572770016c3ce939a5a8aacb44d6f43795ba3a2
suXin <suxinjke@gmail.com>
2019-05-11 00:53:26 +0700

    extract textures; code cleanup

--- game_rsc_extract/game_rsc_extract.js
+++ game_rsc_extract/game_rsc_extract.js
@@ -1,81 +1,76 @@
-const fs = require( 'fs' )
+const fs = require( 'fs' ).promises
 const path = require( 'path' )
 
-const GAME_RSC = fs.readFileSync( process.argv[2] || './GAME.RSC' )
-
-
-
-
-
-
-
 function formatPointer( num = 0 ) {
-    return '0x' + num.toString( 16 ).padStart( 8, 0 )
+    return '0x' + num.toString( 16 ).padStart( 8, '0' )
 }
 
-function parseDOSName( offset ) {
-    return GAME_RSC.slice( offset, offset + 12 ).toString( 'utf8' ).replace( /\u0000/g, '' )
+function parseDOSName( FILE = Buffer.alloc( 0 ), offset = 0 ) {
+    return FILE.slice( offset, offset + 12 ).toString( 'utf8' ).replace( /\u0000/g, '' )
 }
 
-function parseFileEntry( offset ) {
+function parseFileEntry( FILE = Buffer.alloc( 0 ), offset = 0 ) {
     return {
-        // offset: formatPointer( offset ),
-        name: parseDOSName( offset ),
-        // something: buf.readInt32LE( offset + 0xC ),
-        file_offset_table_index: GAME_RSC.readInt16LE( offset + 0x10 ),
-        // weird_offset: buf.readInt16LE( offset + 0x12 )
+        offset: formatPointer( offset ),
+        name: parseDOSName( FILE, offset ),
+        file_offset_table_index: FILE.readInt16LE( offset + 0x10 )
     }
 }
 
-function parseDirectory( offset ) {
-    const directory_ptr = GAME_RSC.readInt32LE( offset )
-    const file_count = GAME_RSC.readInt32LE( offset + 0x4 )
+function parseDirectory( FILE = Buffer.alloc( 0 ), offset = 0 ) {
+    const directory_ptr = FILE.readInt32LE( offset )
+    const file_amount = FILE.readInt32LE( offset + 0x4 )
 
-    const files = [ ... new Array( file_count ) ]
-        .map( ( _, index ) => parseFileEntry( directory_ptr + index * 0x14 ) )
+    const files = [ ... new Array( file_amount ) ]
+        .map( ( _, index ) => parseFileEntry( FILE, directory_ptr + index * 0x14 ) )
 
     return {
         directory_ptr: formatPointer( directory_ptr ),
-        file_count,
+        file_amount,
 
         files
     }
 }
 
-function parseFileOffsets( offset ) {
-    const file_count = GAME_RSC.readInt32LE( offset )
+function parseFileOffsets( FILE = Buffer.alloc( 0 ), offset = 0 ) {
+    const file_amount = FILE.readInt32LE( offset )
 
-    return {
-        file_count,
+    return [ ... new Array( file_amount ) ]
+        .map( ( _, index ) => ( {
+            index,
+            offset: FILE.readInt32LE( ( offset + 0x8 * index ) + 0x4 ),
+            is_texture: FILE.readInt32LE( ( offset + 0x8 * index ) + 0x8 ) === 5
+        } ) )
+        .map( ( file, index, array ) => {
+            const next_file = array[index + 1]
 
-        files: [ ... new Array( file_count ) ]
-            .map( ( _, index ) => ( {
-                index,
-                offset: GAME_RSC.readInt32LE( ( offset + 0x8 * index ) + 0x4 ),
-                something: GAME_RSC.readInt32LE( ( offset + 0x8 * index ) + 0x8 )
-            } ) )
-    }
+            return {
+                ...file,
+                size: next_file ? ( next_file.offset - file.offset ) : 0
+            }
+        } )
 }
 
-function extractOffsets() {
-    const file_offsets_ptr = GAME_RSC.readInt32LE( 0 )
+function parseResources( FILE = Buffer.alloc( 0 ) ) {
+    const file_offsets_ptr = FILE.readInt32LE( 0 )
 
-    const directory_count = 16
+    const directory_amount = 16
 
-    const directories = [ ...new Array( directory_count ) ]
-        .map( ( _, index ) => parseDirectory( 0x4 + index * 0x8 ) )
+    const directories = [ ...new Array( directory_amount ) ]
+        .map( ( _, index ) => parseDirectory( FILE, 0x4 + index * 0x8 ) )
 
-    const file_offsets = parseFileOffsets( file_offsets_ptr ).files
+    const file_offsets = parseFileOffsets( FILE, file_offsets_ptr )
 
     return {
         file_offsets_ptr: formatPointer( file_offsets_ptr ),
+        file_offsets,
 
         directories: directories
             .map( ( directory, index ) => ( {
                 name: (
                     index === 0 ? 'levels' :
                     index === 2 ? 'levels-loading-screens_and_item-descriptions' :
-                    index === 4 ? 'models_maybe' :
+                    index === 4 ? 'models' :
                     index === 5 ? 'dummy_files' :
                     index === 6 ? 'levels_scripts' :
                     index === 8 ? 'menu_related_maybe' :
@@ -90,41 +85,80 @@ function extractOffsets() {
                 ),
                 files: directory.files
             } ) )
-            .filter( directory => directory.files.length > 0 )
+            .filter( directory => directory.files.length > 0 && directory.name !== 'dummy_files' )
             .map( directory => ( {
                 ...directory,
                 files: directory.files.map( file => {
-                    const file_offset = file_offsets[file.file_offset_table_index].offset
+                    const file_offset = file_offsets[file.file_offset_table_index]
                     const next_file = file_offsets[file.file_offset_table_index + 1]
 
                     return {
                         ...file,
-                        file_offset,
-                        file_offset_formatted: formatPointer( file_offset ),
-                        file_size: next_file ? ( next_file.offset - file_offset ) : 0
+                        is_texture: file_offset.is_texture,
+                        file_offset: file_offset.offset,
+                        file_offset_formatted: formatPointer( file_offset.offset ),
+                        file_size: next_file ? ( next_file.offset - file_offset.offset ) : 0
                     }
                 } )
             } ) )
     }
 }
 
-const results = extractOffsets()
+const helpMessage =
+`Colony Wars Red Sun GAME.RSC extractor
+Extracts GAME.RSC contents
 
-const root_dir = path.join( './', 'output' )
+node FILE_extract.js <GAME.RSC path> <output_directory>
+`
 
-results.directories.forEach( directory => {
-    const directory_dir = path.join( root_dir, directory.name )
-    fs.mkdirSync( directory_dir, { recursive: true } )
+async function main() {
+    if ( process.argv.length < 4 ) {
+        console.log( helpMessage )
+        return
+    }
 
-    directory.files.forEach( file => {
-        const file_path = path.join( directory_dir, file.name )
-        const data = GAME_RSC.slice( file.file_offset, file.file_offset + file.file_size )
-        fs.writeFile( file_path, data, ( err ) => {
-            if ( err ) {
-                throw err
-            }
-        } )
+    const [ GAME_RSC_PATH, output_directory ] = process.argv.slice( -2 )
+
+    await fs.mkdir( output_directory, { recursive: true } )
+
+    const GAME_RSC = await fs.readFile( GAME_RSC_PATH )
+
+    const resources = parseResources( GAME_RSC )
+
+    const directory_write_tasks = resources.directories.map( directory => {
+        const directory_path = path.join( output_directory, directory.name )
+        return fs.mkdir( directory_path, { recursive: true } )
+            .then( _ => Promise.all( directory.files.map( file => {
+                const file_path = path.join( directory_path, file.name )
+                const data = GAME_RSC.slice( file.file_offset, file.file_offset + file.file_size )
+                return fs.writeFile( file_path, data )
+            } ) ) )
+            .then( _ => console.log( `Extracted ${directory.name}` ) )
     } )
-} )
 
-fs.writeFileSync( './entries.json', JSON.stringify( results, undefined, 2 ) )
+    const textures_dir = path.join( output_directory, 'textures' )
+    const texture_write_tasks = fs
+        .mkdir( textures_dir, { recursive: true } )
+        .then( _ => {
+            return resources.file_offsets
+                .filter( file_offset => file_offset.is_texture && file_offset.size > 0 )
+                .map( file_offset => {
+                    const file_path = path.join( textures_dir, `TEX_${file_offset.index}.TIM` )
+                    const data = GAME_RSC.slice( file_offset.offset, file_offset.offset + file_offset.size )
+                    return fs.writeFile( file_path, data )
+                } )
+        } )
+
+    Promise.all( [
+        directory_write_tasks,
+        texture_write_tasks
+    ] )
+}
+
+(async () => {
+    try {
+        await main();
+    } catch ( e ) {
+        console.log( e.stack )
+    }
+})()
